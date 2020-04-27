650d385cae975a5c34a0739a304d86dbce5d4707
stikcy-ai <rev.hyeok@gmail.com>
2020-04-27 16:59:33 +0900

    Add preprocessing function

--- preprocessing.py
+++ preprocessing.py
@@ -6,3 +6,19 @@ def drop_na_cols(df):
     na_cols = [df.columns[i] for i in range(len(df.columns) if df.count()[i] < (df.shape[0] / 2))]
     df = df.drop(na_cols, axis = 1)
     return df
+
+
+def drop_na(df, target):
+    """Return not include NA rows"""
+    df = df.dropna(axis=0, subset=target)
+    df = df.dropna(axis=0)
+    return dfh
+
+
+def drop_specific_value(df, target, value):
+    for i in value:
+        df = df.drop(df[df[target] == value].index, axis=0)
+    return df
+
+
+
