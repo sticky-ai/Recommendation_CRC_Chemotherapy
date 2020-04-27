0126a709ea7e47d907ebf7e93e3c57d28af5e947
stikcy-ai <rev.hyeok@gmail.com>
2020-04-27 12:32:22 +0900

    Update file name

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,8 @@
 import numpy as np
 import pandas as pd
 
+def drop_na_cols(df):
+    """Return cols with more than half missing value"""
+    na_cols = [df.columns[i] for i in range(len(df.columns) if df.count()[i] < (df.shape[0] / 2))]
+    df = df.drop(na_cols, axis = 1)
+    return df
