dc27c2540fc6fbcb7a0c8966de2d118b473aafa6
stikcy-ai <rev.hyeok@gmail.com>
2020-07-03 14:57:06 +0900

    First commit

--- preprocessing.py
+++ preprocessing.py
@@ -1,24 +1,40 @@
-import numpy as np
-import pandas as pd
-
-def drop_na_cols(df):
-    """Return cols with more than half missing value"""
-    na_cols = [df.columns[i] for i in range(len(df.columns) if df.count()[i] < (df.shape[0] / 2))]
-    df = df.drop(na_cols, axis = 1)
-    return df
+def init_dataframe(df, feature_idx, row_start_idx):
+    new = df.iloc[row_start_idx:, :]
+    new.columns = df.iloc[feature_idx, :]
+    print('== Dataframe Initialization ==')
+    print('Before : {} / After : {}'.format(df.shape, new.shape))
+    return new
 
 
-def drop_na(df, target):
-    """Return not include NA rows"""
-    df = df.dropna(axis=0, subset=target)
-    df = df.dropna(axis=0)
-    return dfh
+def set_dataframe_priorities(df, priorities):
+    """ type(priorities) == 'list' """
+    new = df[priorities]
+    print('\n== Set Dataframe Only Priority Columns == ')
+    print('Before : {} / After : {}'.format(df.shape, new.shape))
+    return new
 
 
-def drop_specific_value(df, target, value):
-    for i in value:
-        df = df.drop(df[df[target] == value].index, axis=0)
-    return df
+def drop_missing_features(df):
+    drops = [c for c in df.columns if df[c].isnull().sum() > 500 and c != 'Postop_Chemo_Regimen']
+    new = df.drop(drops, axis = 1)
+    print('\n== Missing Features Has Been Dropped ==')
+    for c in drops:
+        print('-', c)
+    print('\nBefore : {} / After : {}'.format(df.shape, new.shape))
+    return new
 
 
+def drop_time_features(df, time_features):
+    new = df.drop(time_features, axis=1)
+    print('\n== Time Features Has Been Dropped ==')
+    for c in time_features:
+        print('-', c)
+    print('\nBefore : {} / After : {}'.format(df.shape, new.shape))
+    return new
 
+
+def drop_NA_instances(df):
+    new = df.dropna()
+    print('\n== Drop NA instances ==')
+    print('Before : {} / After : {}'.format(df.shape, new.shape))
+    return new
